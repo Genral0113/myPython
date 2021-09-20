8b01afe2e9c19ac2052f99b515fdef6be4c8eb20
Genral0113 <licongfa686@126.com>
2021-09-21 01:11:49 +0800

    dlcf@home

--- deepracer_log_analyzer.py
+++ deepracer_log_analyzer.py
@@ -2,6 +2,7 @@ import os
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 
 def read_csv_file(file_name, episode_num=-1):
@@ -77,7 +78,173 @@ def read_csv_file(file_name, episode_num=-1):
     return log_parmas
 
 
-def plot_to_image(file_directory, plt_show=False):
+def multiple_track_line(track_line, multiply_factor=2):
+    new_track_line = []
+
+    for i in range(len(track_line)):
+        # get next point
+        j = divmod(i+1, len(track_line))[1]     # get mod of (i + i) mod len
+
+        prev_point = track_line[i]
+        next_point = track_line[j]
+
+        delta_x = (next_point[0] - prev_point[0]) / multiply_factor
+        delta_y = (next_point[1] - prev_point[1]) / multiply_factor
+
+        for j in range(multiply_factor):
+            temp_point = [prev_point[0] + j * delta_x, prev_point[1] + j * delta_y]
+            new_track_line.append(temp_point)
+
+    if len(new_track_line) != multiply_factor * len(track_line):
+        print('Error in multiplying track line dots .............. ')
+
+    return new_track_line
+
+
+def get_track_direction(track_line):
+    track_direction = []
+
+    for i in range(len(track_line)):
+        # get next point
+        j = divmod(i + 1, len(track_line))[1]    # get mod of (i + i) mod len
+
+        prev_point = track_line[i]
+        next_point = track_line[j]
+
+        # calculate the track angel between [prev_point, middle_point]
+        track_angel = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0])
+        track_angel = math.degrees(track_angel)
+        track_direction.append(track_angel)
+
+    if len(track_direction) != len(track_line):
+        print('Error in calculating track line direction .............. ')
+
+    return track_direction
+
+
+def calculate_distance_of_track_line(track_line):
+    track_distance = []
+
+    for i in range(len(track_line)):
+        # get next point
+        j = divmod(i + 1, len(track_line))[1]  # get mod of (i + i) mod len
+
+        prev_point = track_line[i]
+        next_point = track_line[j]
+
+        distance = dist_2_points(prev_point[0], next_point[0], prev_point[1], next_point[1])
+
+        track_distance.append(distance)
+
+    if len(track_distance) != len(track_line):
+        print('Error in calculating track line distance .............. ')
+
+    return track_distance
+
+
+def dist_2_points(x1, x2, y1, y2):
+    return abs(abs(x1 - x2) ** 2 + abs(y1 - y2) ** 2) ** 0.5
+
+
+def closest_2_racing_points_index(racing_coords, car_coords):
+    # Calculate all distances to racing points
+    distances = []
+    for i in range(len(racing_coords)):
+        distance = dist_2_points(x1=racing_coords[i][0], x2=car_coords[0],
+                                 y1=racing_coords[i][1], y2=car_coords[1])
+        distances.append(distance)
+
+    # Get index of the closest racing point
+    closest_index = distances.index(min(distances))
+
+    # Get index of the second closest racing point
+    distances_no_closest = distances.copy()
+    distances_no_closest[closest_index] = 999
+    second_closest_index = distances_no_closest.index(
+        min(distances_no_closest))
+
+    return [closest_index, second_closest_index]
+
+
+def dist_to_racing_line(closest_coords, second_closest_coords, car_coords):
+    # Calculate the distances between 2 closest racing points
+    a = abs(dist_2_points(x1=closest_coords[0],
+                          x2=second_closest_coords[0],
+                          y1=closest_coords[1],
+                          y2=second_closest_coords[1]))
+
+    # Distances between car and closest and second closest racing point
+    b = abs(dist_2_points(x1=car_coords[0],
+                          x2=closest_coords[0],
+                          y1=car_coords[1],
+                          y2=closest_coords[1]))
+    c = abs(dist_2_points(x1=car_coords[0],
+                          x2=second_closest_coords[0],
+                          y1=car_coords[1],
+                          y2=second_closest_coords[1]))
+
+    # Calculate distance between car and racing line (goes through 2 closest racing points)
+    # try-except in case a=0 (rare bug in DeepRacer)
+    try:
+        distance = abs(-(a ** 4) + 2 * (a ** 2) * (b ** 2) + 2 * (a ** 2) * (c ** 2) -
+                       (b ** 4) + 2 * (b ** 2) * (c ** 2) - (c ** 4)) ** 0.5 / (2 * a)
+    except:
+        distance = b
+
+    return distance
+
+
+# Calculate which one of the closest racing points is the next one and which one the previous one
+def next_prev_racing_point(closest_coords, second_closest_coords, car_coords, heading):
+    # Virtually set the car more into the heading direction
+    heading_vector = [math.cos(math.radians(
+        heading)), math.sin(math.radians(heading))]
+    new_car_coords = [car_coords[0] + heading_vector[0],
+                      car_coords[1] + heading_vector[1]]
+
+    # Calculate distance from new car coords to 2 closest racing points
+    distance_closest_coords_new = dist_2_points(x1=new_car_coords[0],
+                                                x2=closest_coords[0],
+                                                y1=new_car_coords[1],
+                                                y2=closest_coords[1])
+    distance_second_closest_coords_new = dist_2_points(x1=new_car_coords[0],
+                                                       x2=second_closest_coords[0],
+                                                       y1=new_car_coords[1],
+                                                       y2=second_closest_coords[1])
+
+    if distance_closest_coords_new <= distance_second_closest_coords_new:
+        next_point_coords = closest_coords
+        prev_point_coords = second_closest_coords
+    else:
+        next_point_coords = second_closest_coords
+        prev_point_coords = closest_coords
+
+    return [next_point_coords, prev_point_coords]
+
+
+def racing_direction_diff(closest_coords, second_closest_coords, car_coords, heading):
+    # Calculate the direction of the center line based on the closest waypoints
+    next_point, prev_point = next_prev_racing_point(closest_coords,
+                                                    second_closest_coords,
+                                                    car_coords,
+                                                    heading)
+
+    # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
+    track_direction = math.atan2(
+        next_point[1] - prev_point[1], next_point[0] - prev_point[0])
+
+    # Convert to degree
+    track_direction = math.degrees(track_direction)
+
+    # Calculate the difference between the track direction and the heading direction of the car
+    direction_diff = abs(track_direction - heading)
+    if direction_diff > 180:
+        direction_diff = 360 - direction_diff
+
+    return direction_diff
+
+
+def plot_to_image(file_directory, plt_show=0):
     for file_name in os.listdir(file_directory):
         if file_name.split('.')[1] == 'csv':
             file_name_full_path = os.path.join(file_directory, file_name)
@@ -90,20 +257,50 @@ def plot_to_image(file_directory, plt_show=False):
             episode = np.array(log_parmas['episode'])
             x = np.array(log_parmas['x'])
             y = np.array(log_parmas['y'])
+            track_line = []
 
             legent = []
             plt.figure()
             for episode_num in range(max(episode)+1):
                 pos = np.where(episode == episode_num)
-                plt.scatter(x[pos], y[pos], s=2)
-                legent.append('episode='+str(episode_num))
+                track_line.append([x[pos[0][0]], y[pos[0][0]]])
+                if plt_show > 0:
+                    plt.scatter(x[pos], y[pos], s=2)
+                    legent.append('episode='+str(episode_num))
             plt.grid(True)
-            if len(legent) < 6:
+            if len(legent) < 6 and plt_show > 0:
                 plt.legend(legent, loc='upper right')
             plt.title(file_name.split('.')[0])
-            plt.savefig(image_file_name_full_path)
-            if plt_show:
+            if plt_show == 2 or plt_show == 3:
+                plt.savefig(image_file_name_full_path)
+            if plt_show == 1 or plt_show == 3:
                 plt.show()
+
+            if plt_show == 0:
+                x1 = []
+                y1 = []
+                optimal_time = []
+                optimal_speed = 2.5   # 3m/s
+                new_track_line = multiple_track_line(track_line)
+                track_direction = get_track_direction(new_track_line)
+                track_distance = calculate_distance_of_track_line(new_track_line)
+                new_closest_waypoints = closest_2_racing_points_index(new_track_line, [7.22, 1.6])
+                print(new_closest_waypoints)
+                for i in range(len(track_distance)):
+                    optimal_time.append(track_distance[i]/optimal_speed)
+                    x1.append(new_track_line[i][0])
+                    y1.append(new_track_line[i][1])
+                    plt.scatter(new_track_line[i][0], new_track_line[i][1], s=2)
+
+                if len(new_track_line) != len(optimal_time):
+                    print('Error in calculating optimal time .............. ')
+
+                plt.title('track line')
+                plt.show()
+                print(x1)
+                print(y1)
+                print(track_direction)
+                print(optimal_time)
 
 
 if __name__ == '__main__':
@@ -116,4 +313,4 @@ if __name__ == '__main__':
     log_file4_dir = os.path.dirname(__file__) + r'\aws\training-simtrace\dlcf-htc-2021-model6'
     log_file5_dir = os.path.dirname(__file__) + r'\aws\training-simtrace\dlcf-htc-2021-model1'
 
-    plot_to_image(log_file1_dir, plt_show=True)
+    plot_to_image(log_file4_dir, plt_show=0)
--- model7.py
+++ model7.py
@@ -1,67 +1,329 @@
-import math
+class Reward:
+    def __init__(self, verbose=False):
+        self.first_racingpoint_index = None
+        self.verbose = verbose
+        self.maximum_speed = 3  # m/s
+        self.waypoints_multiple = 2
+        self.steps_per_second = 15
+
+    def reward_function(self, params):
+
+        # Import package (needed for heading)
+        import math
+
+        # useful functions
+        def dist_2_points(x1, x2, y1, y2):
+            return abs(abs(x1 - x2) ** 2 + abs(y1 - y2) ** 2) ** 0.5
+
+        def closest_2_racing_points_index(racing_coords, car_coords):
+
+            # Calculate all distances to racing points
+            distances = []
+            for i in range(len(racing_coords)):
+                distance = dist_2_points(x1=racing_coords[i][0], x2=car_coords[0],
+                                         y1=racing_coords[i][1], y2=car_coords[1])
+                distances.append(distance)
+
+            # Get index of the closest racing point
+            closest_index = distances.index(min(distances))
+
+            # Get index of the second closest racing point
+            distances_no_closest = distances.copy()
+            distances_no_closest[closest_index] = 999
+            second_closest_index = distances_no_closest.index(
+                min(distances_no_closest))
+
+            return [closest_index, second_closest_index]
+
+        def dist_to_racing_line(closest_coords, second_closest_coords, car_coords):
+
+            # Calculate the distances between 2 closest racing points
+            a = abs(dist_2_points(x1=closest_coords[0],
+                                  x2=second_closest_coords[0],
+                                  y1=closest_coords[1],
+                                  y2=second_closest_coords[1]))
+
+            # Distances between car and closest and second closest racing point
+            b = abs(dist_2_points(x1=car_coords[0],
+                                  x2=closest_coords[0],
+                                  y1=car_coords[1],
+                                  y2=closest_coords[1]))
+            c = abs(dist_2_points(x1=car_coords[0],
+                                  x2=second_closest_coords[0],
+                                  y1=car_coords[1],
+                                  y2=second_closest_coords[1]))
+
+            # Calculate distance between car and racing line (goes through 2 closest racing points)
+            # try-except in case a=0
+            try:
+                distance = abs(-(a ** 4) + 2 * (a ** 2) * (b ** 2) + 2 * (a ** 2) * (c ** 2) -
+                               (b ** 4) + 2 * (b ** 2) * (c ** 2) - (c ** 4)) ** 0.5 / (2 * a)
+            except:
+                distance = b
+
+            return distance
+
+        # Calculate which one of the closest racing points is the next one and which one the previous one
+        def next_prev_racing_point(closest_coords, second_closest_coords, car_coords, heading):
+
+            # Virtually set the car more into the heading direction
+            heading_vector = [math.cos(math.radians(
+                heading)), math.sin(math.radians(heading))]
+            new_car_coords = [car_coords[0] + heading_vector[0],
+                              car_coords[1] + heading_vector[1]]
+
+            # Calculate distance from new car coords to 2 closest racing points
+            distance_closest_coords_new = dist_2_points(x1=new_car_coords[0],
+                                                        x2=closest_coords[0],
+                                                        y1=new_car_coords[1],
+                                                        y2=closest_coords[1])
+            distance_second_closest_coords_new = dist_2_points(x1=new_car_coords[0],
+                                                               x2=second_closest_coords[0],
+                                                               y1=new_car_coords[1],
+                                                               y2=second_closest_coords[1])
+
+            if distance_closest_coords_new <= distance_second_closest_coords_new:
+                next_point_coords = closest_coords
+                prev_point_coords = second_closest_coords
+            else:
+                next_point_coords = second_closest_coords
+                prev_point_coords = closest_coords
+
+            return [next_point_coords, prev_point_coords]
+
+        def racing_direction_diff(closest_coords, second_closest_coords, car_coords, heading):
+
+            # Calculate the direction of the center line based on the closest waypoints
+            next_point, prev_point = next_prev_racing_point(closest_coords,
+                                                            second_closest_coords,
+                                                            car_coords,
+                                                            heading)
+
+            # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
+            track_direction = math.atan2(
+                next_point[1] - prev_point[1], next_point[0] - prev_point[0])
+
+            # Convert to degree
+            track_direction = math.degrees(track_direction)
+
+            # Calculate the difference between the track direction and the heading direction of the car
+            direction_diff = abs(track_direction - heading)
+            if direction_diff > 180:
+                direction_diff = 360 - direction_diff
+
+            return direction_diff
+
+        # Gives back indexes that lie between start and end index of a cyclical list
+        # (start index is included, end index is not)
+        def indexes_cyclical(start, end, array_len):
+
+            if end < start:
+                end += array_len
+
+            return [index % array_len for index in range(start, end)]
+
+        # Calculate how long car would take for entire lap, if it continued like it did until now
+        def projected_time(first_index, closest_index, step_count, times_list):
+
+            # Calculate how much time has passed since start
+            current_actual_time = (step_count - 1) / self.steps_per_second
+
+            # Calculate which indexes were already passed
+            indexes_traveled = indexes_cyclical(first_index, closest_index, len(times_list))
+
+            # Calculate how much time should have passed if car would have followed optimals
+            current_expected_time = sum([times_list[i] for i in indexes_traveled])
+
+            # Calculate how long one entire lap takes if car follows optimals
+            total_expected_time = sum(times_list)
+
+            # Calculate how long car would take for entire lap, if it continued like it did until now
+            try:
+                projected_time = (current_actual_time / current_expected_time) * total_expected_time
+            except:
+                projected_time = 9999
+
+            return projected_time
+
+        # generate new track line by multiplying the waypoints
+        def multiple_track_line(track_line, multiply_factor=2):
+            new_track_line = []
+
+            for i in range(len(track_line)):
+                # get next point
+                j = divmod(i + 1, len(track_line))[1]  # get mod of (i + i) mod len
+
+                prev_point = track_line[i]
+                next_point = track_line[j]
+
+                delta_x = (next_point[0] - prev_point[0]) / multiply_factor
+                delta_y = (next_point[1] - prev_point[1]) / multiply_factor
+
+                for j in range(multiply_factor):
+                    temp_point = [prev_point[0] + j * delta_x, prev_point[1] + j * delta_y]
+                    new_track_line.append(temp_point)
+
+            if len(new_track_line) != multiply_factor * len(track_line):
+                print('Error in multiplying track line dots .............. ')
+
+            return new_track_line
+
+        # calculate distance of 2 points in a track line
+        def calculate_distance_of_track_line(track_line):
+            track_distance = []
+
+            for i in range(len(track_line)):
+                # get next point
+                j = divmod(i + 1, len(track_line))[1]  # get mod of (i + i) mod len
+
+                prev_point = track_line[i]
+                next_point = track_line[j]
+
+                distance = dist_2_points(prev_point[0], next_point[0], prev_point[1], next_point[1])
+
+                track_distance.append(distance)
+
+            if len(track_distance) != len(track_line):
+                print('Error in calculating track line distance .............. ')
+
+            return track_distance
+
+        ################## INPUT PARAMETERS ###################
+
+        # Read all input parameters
+        all_wheels_on_track = params['all_wheels_on_track']
+        x = params['x']
+        y = params['y']
+        distance_from_center = params['distance_from_center']
+        is_left_of_center = params['is_left_of_center']
+        heading = params['heading']
+        progress = params['progress']
+        steps = params['steps']
+        speed = params['speed']
+        steering_angle = params['steering_angle']
+        track_width = params['track_width']
+        waypoints = params['waypoints']
+        closest_waypoints = params['closest_waypoints']
+        is_offtrack = params['is_offtrack']
+
+        #################### RACING LINE ######################
+
+        # Optimal racing line with maximum speed
+        # Each row: [x,y,speed,timeFromPreviousPoint]
+        racing_track = []
+        track_line = multiple_track_line(waypoints, multiply_factor=3)
+        track_distance = calculate_distance_of_track_line(track_line)
+        for i in range(track_line):
+            optimal_speed = self.maximum_speed
+            optimal_time = track_distance[i] / optimal_speed
+            racing_track.append([track_line[i][0], track_line[i][1], optimal_speed, optimal_time])
+
+        ############### OPTIMAL X,Y,SPEED,TIME ################
+
+        # Get closest indexes for racing line (and distances to all points on racing line)
+        closest_index, second_closest_index = closest_2_racing_points_index(racing_track, [x, y])
+
+        # Get optimal [x, y, speed, time] for closest and second closest index
+        optimals = racing_track[closest_index]
+        optimals_second = racing_track[second_closest_index]
+
+        # Save first racingpoint of episode for later
+        if self.verbose == True:
+            self.first_racingpoint_index = 0  # this is just for testing purposes
+        if steps == 1:
+            self.first_racingpoint_index = closest_index
+
+        ################ REWARD AND PUNISHMENT ################
+
+        ## Define the default reward ##
+        reward = 1
+
+        ## Reward if car goes close to optimal racing line ##
+        DISTANCE_MULTIPLE = 1
+        dist = dist_to_racing_line(optimals[0:2], optimals_second[0:2], [x, y])
+        distance_reward = max(1e-3, 1 - (dist / (track_width * 0.5)))
+        reward += distance_reward * DISTANCE_MULTIPLE
+
+        ## Reward if speed is close to optimal speed ##
+        SPEED_DIFF_NO_REWARD = 1
+        SPEED_MULTIPLE = 2
+        speed_diff = abs(optimals[2] - speed)
+        if speed_diff <= SPEED_DIFF_NO_REWARD:
+            # we use quadratic punishment (not linear) bc we're not as confident with the optimal speed
+            # so, we do not punish small deviations from optimal speed
+            speed_reward = (1 - (speed_diff / (SPEED_DIFF_NO_REWARD)) ** 2) ** 2
+        else:
+            speed_reward = 0
+        reward += speed_reward * SPEED_MULTIPLE
+
+        # Reward if less steps
+        REWARD_PER_STEP_FOR_FASTEST_TIME = 1
+        STANDARD_TIME = 13
+        FASTEST_TIME = 9
+        times_list = [row[3] for row in racing_track]
+        projected_time = projected_time(self.first_racingpoint_index, closest_index, steps, times_list)
+        try:
+            steps_prediction = projected_time * self.steps_per_second + 1
+            reward_prediction = max(1e-3, (-REWARD_PER_STEP_FOR_FASTEST_TIME * (FASTEST_TIME) /
+                                           (STANDARD_TIME - FASTEST_TIME)) * (
+                                                steps_prediction - (STANDARD_TIME * self.steps_per_second + 1)))
+            steps_reward = min(REWARD_PER_STEP_FOR_FASTEST_TIME, reward_prediction / steps_prediction)
+        except:
+            steps_reward = 0
+        reward += steps_reward
+
+        # Zero reward if obviously wrong direction (e.g. spin)
+        direction_diff = racing_direction_diff(
+            optimals[0:2], optimals_second[0:2], [x, y], heading)
+        if direction_diff > 30:
+            reward = 1e-3
+
+        # punish if steer angel is big
+        if abs(steering_angle) > 15:
+            reward *= 0.8
+
+        # Zero reward of obviously too slow
+        speed_diff_zero = optimals[2] - speed
+        if speed_diff_zero > 0.5:
+            reward = 1e-3
+
+        ## Incentive for finishing the lap in less steps ##
+        REWARD_FOR_FASTEST_TIME = 1500  # should be adapted to track length and other rewards
+        STANDARD_TIME = 13  # seconds (time that is easily done by model)
+        FASTEST_TIME = 9  # seconds (best time of 1st place on the track)
+        if progress == 100:
+            finish_reward = max(1e-3, (-REWARD_FOR_FASTEST_TIME /
+                                       (self.steps_per_second * (STANDARD_TIME - FASTEST_TIME))) * (steps - STANDARD_TIME * self.steps_per_second))
+        else:
+            finish_reward = 0
+        reward += finish_reward
+
+        ## Zero reward if off track ##
+        if all_wheels_on_track == False:
+            reward = 1e-3
+
+        ####################### VERBOSE #######################
+
+        if self.verbose == True:
+            print("Closest index: %i" % closest_index)
+            print("Distance to racing line: %f" % dist)
+            print("=== Distance reward (w/out multiple): %f ===" % (distance_reward))
+            print("Optimal speed: %f" % optimals[2])
+            print("Speed difference: %f" % speed_diff)
+            print("=== Speed reward (w/out multiple): %f ===" % speed_reward)
+            print("Direction difference: %f" % direction_diff)
+            print("Predicted time: %f" % projected_time)
+            print("=== Steps reward: %f ===" % steps_reward)
+            print("=== Finish reward: %f ===" % finish_reward)
+
+        #################### RETURN REWARD ####################
+
+        # Always return a float value
+        return float(reward)
+
+
+reward_object = Reward()  # add parameter verbose=True to get noisy output for testing
 
 
 def reward_function(params):
-    # Define constants
-    standard_speed = 1.0
-    optimal_speed = 1.5
-    fastest_speed = 2.0
-    track_length = 17.71
-    laps = 3
-
-    # Read all input parameters
-    all_wheels_on_track = params['all_wheels_on_track']
-    x = params['x']
-    y = params['y']
-    distance_from_center = params['distance_from_center']
-    is_left_of_center = params['is_left_of_center']
-    heading = params['heading']
-    progress = params['progress']
-    steps = params['steps']
-    speed = params['speed']
-    steering_angle = params['steering_angle']
-    track_width = params['track_width']
-    waypoints = params['waypoints']
-    closest_waypoints = params['closest_waypoints']
-    is_offtrack = params['is_offtrack']
-    is_left_of_center = params['is_left_of_center']
-
-    ## Define the default reward ##
-    reward = 1
-
-    ## Reward if car goes close to optimal racing line - wayponts line ##
-    DISTANCE_MULTIPLE = 1
-    distance_reward = max(1e-3, 1 - (distance_from_center / (track_width * 0.5)))
-    reward += distance_reward * DISTANCE_MULTIPLE
-
-   # Zero reward if obviously wrong direction (e.g. spin)
-    next_point = waypoints[closest_waypoints[1]]
-    prev_point = waypoints[closest_waypoints[0]]
-
-    # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
-    track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0])
-
-    # Convert to degree
-    track_direction = math.degrees(track_direction)
-
-    direction_diff = abs(track_direction - heading)
-    if direction_diff > 180:
-        direction_diff = 360 - direction_diff
-    if direction_diff > 20:
-        reward = 1e-3
-
-    ## keep small angel of the headng direction ##
-    if abs(steering_angle) > 10:
-        reward *= 0.8
-
-    # Zero reward of obviously too slow
-    speed_diff_zero = optimal_speed - speed
-    if speed_diff_zero > 0.5:
-        reward = 1e-3
-
-    ## Zero reward if off track ##
-    if all_wheels_on_track == False:
-        reward = 1e-3
-
-    # Always return a float value
-    return float(reward)
+    return reward_object.reward_function(params)
