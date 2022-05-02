49b335379f7d248950af06cebb40ea204b9727fa
Genral0113 <licongfa686@126.com>
2022-05-02 18:03:22 +0800

    dlcf@home

--- bank_card_number_detecter.py
+++ bank_card_number_detecter.py
@@ -17,8 +17,15 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="path to input image")
 ap.add_argument("-t", "--template", required=True, help="path to template OCR-A image")
 ap.add_argument("-min", "--min_value", required=True, help="input minimum value")
+ap.add_argument("-d", "--debug", required=True, help="input if debug")
 args = vars(ap.parse_args())
-print(args)
+if args['debug'] == 'True':
+    args['debug'] = True
+else:
+    args['debug'] = False
+
+if args['debug']:
+    print(args)
 
 #
 FIRST_NUMBER = {
@@ -30,24 +37,139 @@ FIRST_NUMBER = {
 
 
 def cv_show(name, img):
+    cv2.namedWindow(name, cv2.WINDOW_NORMAL)
     cv2.imshow(name, img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
 img = cv2.imread(args['template'])
-cv_show('original image', img)
+if args['debug']:
+    cv_show('original template', img)
 
 ref = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv_show('ref', ref)
+if args['debug']:
+    cv_show('ref', ref)
 
-ref = cv2.threshold(ref, int(args['min_value']), 255, cv2.THRESH_BINARY)[1]
-cv_show('binary image', ref)
+ref_binary = cv2.threshold(ref, int(args['min_value']), 255, cv2.THRESH_BINARY_INV)[1]
+if args['debug']:
+    cv_show('binary image', ref_binary)
 
-kernel = np.ones((3, 3), np.uint8)
-ref = cv2.erode(ref, kernel, iterations=1)
-cv_show('erode', ref)
+# kernel = np.ones((3, 3), np.uint8)
+# ref = cv2.erode(ref, kernel, iterations=1)
+# if args['debug']:
+    # cv_show('erode', ref)
 
-ref_, refCnts, hierarchy = cv2.findContours(ref.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cv2.drawContours(img, refCnts, -1, (0, 0, 255), 3)
-cv_show('img', img)
+ref_, refCnts, hierarchy = cv2.findContours(ref_binary.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contour_img = cv2.drawContours(img.copy(), refCnts, -1, (0, 0, 255), 1)
+if args['debug']:
+    cv_show('contour', contour_img)
+
+refCnts = myutils.sort_contours(refCnts, method="left-to-right")[0]
+
+digits = {}
+for (i, c) in enumerate(refCnts):
+    (x, y, w, h) = cv2.boundingRect(c)
+    roi = ref[y:y + h, x:x + w]
+    roi = cv2.resize(roi, (57, 88))
+    digits[i] = roi
+
+if args['debug']:
+    for i in range(len(digits)):
+        cv_show(str(i), digits[i])
+
+rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 3))
+sqKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+
+image = cv2.imread(args["image"])
+if args['debug']:
+    cv_show('image', image)
+image = myutils.resize(image, width=300)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+if args['debug']:
+    cv_show('gray', gray)
+
+tophat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, rectKernel)
+if args['debug']:
+    cv_show('tophat', tophat)
+
+gradX = cv2.Sobel(tophat, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=-1)
+
+gradX = np.absolute(gradX)
+(minVal, maxVal) = (np.min(gradX), np.max(gradX))
+gradX = (255 * ((gradX - minVal) / (maxVal - minVal)))
+gradX = gradX.astype("uint8")
+
+print(np.array(gradX).shape)
+if args['debug']:
+    cv_show('gradX', gradX)
+
+gradX = cv2.morphologyEx(gradX, cv2.MORPH_CLOSE, rectKernel)
+if args['debug']:
+    cv_show('gradX', gradX)
+thresh = cv2.threshold(gradX, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+if args['debug']:
+    cv_show('thresh', thresh)
+
+thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, sqKernel)
+if args['debug']:
+    cv_show('thresh', thresh)
+
+thresh_, threshCnts, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = threshCnts
+cur_img = image.copy()
+cv2.drawContours(cur_img, cnts, -1, (0, 0, 255), 3)
+if args['debug']:
+    cv_show('img', cur_img)
+
+locs = []
+for (i, c) in enumerate(cnts):
+    (x, y, w, h) = cv2.boundingRect(c)
+    ar = w / float(h)
+
+    if 2.5 < ar < 4.0:
+        if (40 < w < 55) and (10 < h < 20):
+            locs.append((x, y, w, h))
+
+locs = sorted(locs, key=lambda x: x[0])
+
+output = []
+
+for (i, (gX, gY, gW, gH)) in enumerate(locs):
+    groupOutput = []
+    group = gray[gY-5:gY + gH + 5, gX - 5: gX + gW + 5]
+    if args['debug']:
+        cv_show('group', group)
+
+    group = cv2.threshold(group, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    if args['debug']:
+        cv_show('group', group)
+
+    group_, digitCnts, hierarchy = cv2.findContours(group.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    digitCnts = contours.sort_contours(digitCnts, method="left-to-right")[0]
+
+    for c in digitCnts:
+        (x, y, w, h) = cv2.boundingRect(c)
+        roi = group[y:y + h, x:x + w]
+        roi = cv2.resize(roi, (57, 88))
+        if args['debug']:
+            cv_show('roi', roi)
+
+        scores = []
+
+        for(digit, digitROI) in digits.items():
+            result = cv2.matchTemplate(roi, digitROI, cv2.TM_CCOEFF)
+            (_, score, _, _) = cv2.minMaxLoc(result)
+            scores.append(score)
+
+    groupOutput.append(str(np.argmax(scores)))
+
+    cv2.rectangle(image, (gX - 5, gY - 5), (gX + gW + 5, gY + gH + 5), (0, 0, 255), 1)
+    cv2.putText(image, "".join(groupOutput), (gX, gY - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 0, 255), 2)
+
+    output.extend(groupOutput)
+
+# print('Credit Card Type: {}'.format(FIRST_NUMBER[output[0]]))
+# print('Credit Card # {}'.format(''.join(output)))
+cv2.imshow('Image', image)
+cv2.waitKey(0)
---
+++ myutils.py
@@ -0,0 +1,34 @@
+import cv2
+
+
+def sort_contours(cnts, method="left-to-right"):
+    reverse = False
+    i = 0
+
+    if method == "right-to-left" or method == "bottom-to-top":
+        reverse = True
+
+    if method == "top-to-bottom" or method == "bottom-to-top":
+        i = 1
+
+    bounding_boxes = [cv2.boundingRect(c) for c in cnts]
+
+    (cnts, bounding_boxes) = zip(*sorted(zip(cnts, bounding_boxes), key=lambda b: b[1][i], reverse=reverse))
+
+    return cnts, bounding_boxes
+
+
+def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+    if width is None and height is None:
+        return image
+
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+    resized = cv2.resize(image, dim, interpolation=inter)
+    return resized
