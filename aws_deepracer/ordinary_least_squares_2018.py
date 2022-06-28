f1055b5eb17560b1f3ce01c175e1b4276f16c050
Genral0113 <licongfa686@126.com>
2022-06-28 22:16:13 +0800

    dlcf@home

--- aws_deepracer/ordinary_least_squares_2018.py
+++ aws_deepracer/ordinary_least_squares_2018.py
@@ -2,10 +2,6 @@ import math
 
 
 def reward_function(params):
-    '''
-    Example of rewarding the agent to follow center line
-    '''
-
     # Read input parameters
     track_width = params['track_width']
     distance_from_center = params['distance_from_center']
@@ -13,7 +9,6 @@ def reward_function(params):
     y = params['y']
     speed = params['speed']
     steering = params['steering_angle']
-    # 前面是定义了一些会用的参数
 
     y1 = -0.1978348 * x + 1.8547
     y2 = y1 + 0.1
@@ -23,18 +18,15 @@ def reward_function(params):
     else:
         p1 = 1e-3
 
-    # 这就是先求出函数，在用线性来给他赋reward
-
-    s1 = speed ** 2
     # 速度就是很基本的速度平方
+    s1 = speed ** 2
 
+    # 角度注意steering angle 是左正右负
     STEERING_THRESHOLD = -10
-
     if STEERING_THRESHOLD < steering < 0:
         a1 = 3.0
     else:
         a1 = 1e-3
-    # 角度注意steering angle 是左正右负
 
     reward = p1 * 3 + s1 * 0.5 + a1 * 3
 
